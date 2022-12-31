1f460f42b74774911c8ccc4212716114b81991ef
Thomas Bono <thomasbono.com@gmail.com>
2022-12-31 01:20:50 -0500

    cleaned up and ready for the next component

--- components/sb-quote.vue
+++ components/sb-quote.vue
@@ -10,9 +10,15 @@ const richText = computed(() =>
 //const richText = computed(() => renderRichText(props.blok.quote))
 const photo = ref(props.blok.photo.filename ? props.blok.photo : null)
 const author = ref(props.blok.author)
+const fullWidth = ref(props.blok.full_width)
 </script>
 <template>
-  <div v-if="blok" v-editable="blok" class="blok quote-blok mb-3">
+  <div
+    v-if="blok"
+    v-editable="blok"
+    class="blok quote-blok mb-3"
+    :class="[{ 'full-width': fullWidth }]"
+  >
     <div class="grid">
       <div
         v-if="photo"
@@ -21,12 +27,19 @@ const author = ref(props.blok.author)
         <sb-image :src="photo" size="216x216" />
       </div>
       <div
-        class="flex align-items-center"
+        class="
+          flex
+          align-items-center
+          justify-content-center
+          sm:justify-content-start
+        "
         :class="photo ? 'col-12 sm:col-9 sm:pl-3 pt-3 sm:pt-0' : 'col-12'"
       >
         <div class="pl-0">
-          <div class="quote" v-html="richText" />
-          <p v-if="author" class="author p-small mt-3">&mdash; {{ author }}</p>
+          <div class="quote text-center sm:text-left" v-html="richText" />
+          <p v-if="author" class="author p-small mt-3 text-center sm:text-left">
+            &mdash; {{ author }}
+          </p>
         </div>
       </div>
     </div>
